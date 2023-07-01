2e472d8054c53d9b353209c61fa34d5049f90170
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-07-01 12:21:40 -0700

    Updated for full day

--- script/data_continuity.py
+++ script/data_continuity.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from datetime import datetime
 
@@ -5,45 +6,47 @@ import plotly.express as px
 import plotly.io as pio
 pio.renderers.default = 'browser'
 
-path = r"D:\GitHub\match_events\data\20221206_ISR_19Ave\data_raw.txt"
-df = pd.read_csv(path, sep = '\t')
-print(df.shape)
+os.chdir(r"D:\SynologyDrive\Data\High Resolution Events Data\Indian School")
 
+# read file
+year, month, day = 2022, 12, 14
+file = str(year) + str(month).zfill(2) + str(day).zfill(2)
+df = pd.read_csv(file + "_raw.txt", sep = '\t')
+
+# filter intersection, convert timestamp
+ID = 46
+df = df[df.DeviceID == ID]
+df.drop('DeviceID', axis = 1, inplace = True)
 df.TimeStamp = pd.to_datetime(df.TimeStamp, format = '%m-%d-%Y %H:%M:%S.%f')
 
-# detector configuration
-det_wb_thru_adv = (27, 28, 29)
-det_wb_thru_stop = (9, 10, 11)
-det_wb_lt_stop = (5, 6)
-det = det_wb_thru_adv + det_wb_thru_stop + det_wb_lt_stop
+# detector configuration for wb
+det = {'adv': (27, 28, 29), 
+       'stop': (9, 10, 11), 
+       'left': (5, 6)}
+det_set = det['adv'] + det['stop'] + det['left']
 
 # filter events and detector parameters
 fdf = df.copy(deep = True)
-fdf = fdf.loc[(fdf.EventID == 82) & (fdf.Parameter.isin(det))]
+fdf = fdf.loc[(df.EventID == 82) & (df.Parameter.isin(det_set))]
 fdf.Parameter = fdf.Parameter.astype(str)
 
 # plot data continuity for whole dataset
 det_order = {'Parameter': sorted(det)}
-px.scatter(
+fig = px.scatter(
     fdf, x = 'TimeStamp', y = 'Parameter', 
     category_orders = det_order
-).show()
+)
+fig.show()
 
-# filter timestamp for test period: December 6, 2022; 7:45 to 8:15
-from_time = datetime(2022, 12, 6, 7, 45)
-to_time = datetime(2022, 12, 6, 8, 15)
-ndf = df[(df.TimeStamp >= from_time) & (df.TimeStamp <= to_time)]
+output_fig = os.path.join(r"D:\GitHub\match_events\output\actuation_html", file + "_raw.html")
+fig.write_html(output_fig)
 
-# ndf.to_csv(r"D:\GitHub\match_events\data\20221206_ISR_19Ave\data_process.txt", sep = '\t', index = False)
+# filter timestamp for data subset
+start = datetime(year, month, day, 9, 45)
+end = datetime(year, month, day, 10, 15)
+sdf = df[(df.TimeStamp >= start) & (df.TimeStamp <= end)]
 
-# detection count by lane
-sdf = ndf.copy(deep = True)
-sdf = sdf.loc[(sdf.EventID == 82) & (sdf.Parameter.isin(det))]
-sdf.Parameter = sdf.Parameter.astype(str)
-print(sdf.Parameter.value_counts().sort_values())
+output_period = str(start.hour).zfill(2) + str(start.minute).zfill(2) + '_' + str(end.hour).zfill(2) + str(end.minute).zfill(2)
 
-# plot detection points for subset
-px.scatter(
-    sdf, x = 'TimeStamp', y = 'Parameter', 
-    category_orders = det_order
-).show()
+output_file = os.path.join(r"D:\GitHub\match_events\ignore\data", file + '_' + output_period + ".txt")
+sdf.to_csv(output_file, sep = '\t', index = False)
