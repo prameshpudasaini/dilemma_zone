95ae493eb28e33e336ee86f226735b3c20b535c0
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-07-16 13:50:57 -0700

    Updated matching algorithm

--- script/match_events.py
+++ script/match_events.py
@@ -1,6 +1,7 @@
 import os
+import ast
 import pandas as pd
-import statistics as st
+import collections
 
 os.chdir(r"D:\GitHub\dilemma_zone")
 
@@ -12,30 +13,18 @@ class Vector():
         return repr(self.data)
     def __sub__(self, other):
         return list((a-b).total_seconds() for a, b in zip(self.data, other.data))
+    
+# read results of video-verified matches
+with open('data/calibration/manual_adv_left_pairs.txt') as f:
+    left_result = ast.literal_eval(f.read())
 
-# select period and file
-def getFileName(year, month, day, from_hour, from_min, to_hour, to_min):
-    return(str(year) + str(month).zfill(2) + str(day).zfill(2) + '_' +
-           str(from_hour).zfill(2) + str(from_min).zfill(2) + '_' +
-           str(to_hour).zfill(2) + str(to_min).zfill(2))
-
-# read file, convert timestamp
-file = getFileName(2023, 3, 27, 14, 15, 18, 45)
-path = "ignore/calibration_data"
-
-df = pd.read_csv(os.path.join(path, file + '_filtered.txt'), sep = '\t')
-df.TimeStamp = pd.to_datetime(df.TimeStamp, format = '%Y-%m-%d %H:%M:%S.%f').sort_values()
-
-# data frames for adv, stop, left-turn
-adf = df[df.Det == 'adv']
-sdf = df[df.Det == 'stop']
-ldf = df[df.Det == 'rear']
-
-# actuation IDs
-set_id_adv = set(adf.ID)
-set_id_stop = set(sdf.ID)
-set_id_left = set(ldf.ID)
-
+with open('data/calibration/manual_adv_stop_pairs.txt') as f:
+    thru_result = ast.literal_eval(f.read())
+    
+# store accuracy parameters
+left_accuracy = collections.defaultdict(list)
+thru_accuracy = collections.defaultdict(list)
+    
 # =============================================================================
 # parameters
 # =============================================================================
@@ -48,37 +37,70 @@ dist_adv_stop = dist_det - len_stop
 
 # speed
 speed_limit = round(35*5280/3600, 1)
-speed_max = round(50*5280/3600, 1)
 
-# min, max of through travel time
-dec = round((speed_limit**2) / (2*dist_det), 1) # constant stopping dec for veh traveling at speed limit
-tt_thru_min = round(dist_adv_stop / speed_max, 1)
-tt_thru_max = round(speed_limit / dec, 1)
+# empirical min, max of through travel time
+tt_thru_min = 3
+tt_thru_max = 11
 
 # empirical travel time to stop/run over stop-bar det
-tt_thru_ideal_stop = 6.5
-tt_thru_ideal_run = 4.4
+tt_thru_ideal_stop = 6.6
+tt_thru_ideal_run = 4.6
 
 # empirical min, max, ideal left-turn travel time (from adv det to left-turn lane rear det)
-tt_left_min = 4.5
-tt_left_max = 6.5
-tt_left_ideal = 5.5
-
-# =============================================================================
-# match events: advance det to left-turn rear det
-# =============================================================================
-
-# initial candidates match of adv to left-turn
-left_match_initial = []
-
-for i in sorted(set_id_adv):
-    adv_lane = adf[adf.ID == i].Lane.values[0].astype(int)
+tt_left_min = 4
+tt_left_max = 7
+tt_left_ideal = 5.4
     
-    if adv_lane == 2: # consider left-turn from only left-most thru lane
+# =============================================================================
+# read data
+# =============================================================================
+
+num = 5 # file number (6 files in total for calibration)
+
+# select period and file
+def getFileName(year, month, day, from_hour, from_min, to_hour, to_min):
+    return(str(year) + str(month).zfill(2) + str(day).zfill(2) + '_' +
+           str(from_hour).zfill(2) + str(from_min).zfill(2) + '_' +
+           str(to_hour).zfill(2) + str(to_min).zfill(2))
+
+# read file, convert timestamp
+path = "ignore/calibration_data"
+files = list([getFileName(2022, 12, 6, 7, 45, 8, 15),
+              getFileName(2022, 12, 6, 8, 45, 9, 15),
+              getFileName(2022, 12, 14, 7, 45, 8, 15),
+              getFileName(2022, 12, 14, 8, 45, 9, 15),
+              getFileName(2022, 12, 14, 9, 45, 10, 15),
+              getFileName(2023, 3, 27, 14, 15, 18, 45)])
+
+df = pd.read_csv(os.path.join(path, files[num] + '_filtered.txt'), sep = '\t')
+df.TimeStamp = pd.to_datetime(df.TimeStamp, format = '%Y-%m-%d %H:%M:%S.%f').sort_values()
+
+# data frames for adv, stop, left-turn
+adf = df[df.Det == 'adv']
+sdf = df[df.Det == 'stop']
+ldf = df[df.Det == 'rear']
+
+# actuation IDs
+set_id_adv = set(sorted(adf.ID))
+set_id_stop = set(sorted(sdf.ID))
+set_id_rear = set(sorted(ldf.ID))
+
+set_id_adv_left = set(sorted(adf[adf.Lane == 2].ID))
+
+# =============================================================================
+# function to match events: adv det to left-turn rear det
+# =============================================================================
+
+def processCandidateMatchesAdvLeft():
+    print("Processing candidate match pairs from adv to rear det \n")
+    
+    left_match_initial = [] # initial candidate match pairs of adv to left-turn
+    
+    for i in set_id_adv_left:
         adv_time = adf[adf.ID == i].TimeStamp
     
         left_candidate = {}        
-        for k in sorted(set_id_left):
+        for k in set_id_rear:
             left_time = ldf[ldf.ID == k].TimeStamp
             
             if left_time.values[0] > adv_time.values[0]: # look forward in timestamp
@@ -98,56 +120,134 @@ for i in sorted(set_id_adv):
         
         if len(left_candidate) != 0: # consider only non-empty candidate matches
             left_match_initial.append(left_candidate)
-
-# final candidates match of adv to left-turn based on highest matching strength
-left_match_final = {}
-seen_left_id = []
-
-for candidate in left_match_initial:
-    current_match_strength = 0
+                
+    return left_match_initial
     
-    for key, value in candidate.items():
-        current_left_id = key
+# =============================================================================
+# function to process candidate match pairs
+# =============================================================================
+
+def processFinalMatchPairs(match_initial):
+    print("Processing final match pairs \n")
+    
+    # convert list of nested dictionaries to flat list
+    match_flat = []
+    for candidate in match_initial:
+        for key, value in candidate.items():
+            match_flat.append({key: value})
+            
+    match_final = {}
+    seen_target_id, seen_adv_id = [], []
+    seen_match_strength_target, seen_match_strength_adv = {}, {}
+    
+    # function to update match pairs based on highest matching strength
+    def updateMatchPairs():
+        match_final[current_target_id] = list([current_adv_id, current_match_strength])
+        seen_match_strength_target[current_target_id] = current_match_strength
+        seen_match_strength_adv[current_adv_id] = current_match_strength
+        return None
+        
+    for item in match_flat:
+        key = list(item.keys())
+        value = list(item.values())[0]
+        
+        current_target_id = key[0]
         current_adv_id = value[0]
         current_match_strength = value[1]
         
-        if current_left_id in seen_left_id: # this left id already found a match with adv det
-            seen_match_strength = left_match_final[current_left_id][1] # update seen match strength
-            if current_match_strength > seen_match_strength: # check which match strength is higher
-                left_match_final[current_left_id] = list([current_adv_id, current_match_strength])
-        else:
-            seen_left_id.append(current_left_id)
-            left_match_final[current_left_id] = list([current_adv_id, current_match_strength])
+        if current_target_id not in seen_target_id:
+            if current_adv_id not in seen_adv_id:
+                seen_target_id.append(current_target_id)
+                seen_adv_id.append(current_adv_id)
+                updateMatchPairs()
+            else: # current adv id already seen
+                seen_match_strength = seen_match_strength_adv[current_adv_id]
+                if current_match_strength > seen_match_strength:
+                    updateMatchPairs()
+        else: # current target id already seen
+            if current_adv_id not in seen_adv_id:
+                seen_match_strength = seen_match_strength_target[current_target_id]
+                if current_match_strength > seen_match_strength:
+                    updateMatchPairs()
+            else: # current target id and current adv id both seen
+                # look for greater matching strength out of adv and target
+                seen_match_strength = max(seen_match_strength_target[current_target_id],
+                                          seen_match_strength_adv[current_adv_id])
+                if current_match_strength > seen_match_strength:
+                    updateMatchPairs()
+                    
+    return match_final
 
-# list of actuation IDs over adv det that took left-turn
-seen_adv_id = []
-for i in seen_left_id:
-    seen_adv_id.append(left_match_final[i][0])
+# =============================================================================
+# analysis of adv-rear match pairs
+# =============================================================================
+
+left_match_initial = processCandidateMatchesAdvLeft()
+left_match_final = processFinalMatchPairs(left_match_initial)
+
+# final match of adv & left id pairs
+left_match_pairs = {}
+for key, value in left_match_final.items():
+    left_match_pairs[value[0]] = key
+
+# set of unseen acutation ids at adv det
+seen_adv_id = set(left_match_pairs.keys())
+unseen_adv_id = set_id_adv_left - seen_adv_id
+
+# update key-value pairs of unseen adv ids
+left_match_pairs_full = left_match_pairs.copy()
+for i in unseen_adv_id:
+    left_match_pairs_full[i] = 0
+left_match_pairs_full = dict(sorted(left_match_pairs_full.items()))
+
+left_result_pairs = left_result[num+1]
+
+# correct pairs in both match & result identified as left-turning
+TP_pairs = dict(set(left_match_pairs.items()).intersection(left_result_pairs.items()))
+
+# pairs in match not present in result (falsely classified as left-turning)
+FP_pairs = dict(sorted(set(left_match_pairs.items()).difference(left_result_pairs.items())))
+
+# pairs in result but not found in match (falsely classified as thru going)
+FN_pairs = dict(sorted(set(left_result_pairs.items()).difference(left_match_pairs_full.items())))
+
+# compute precision & recall
+TP = len(TP_pairs)
+FP = len(FP_pairs)
+FN = len(FN_pairs)
+TN = len(set_id_adv_left) - TP - FP - FN
+
+# append accuracy parameters to dictionary
+left_accuracy['TP'].append(TP)
+left_accuracy['FP'].append(FP)
+left_accuracy['FN'].append(FN)
+left_accuracy['TN'].append(TN)
+
+# =============================================================================
+# function to match events: adv det to stop-bar det
+# =============================================================================
+
+set_id_adv_look = set_id_adv - seen_adv_id
+        
+def processCandidateMatchesAdvStop():
+    print("Processing candidate match pairs from adv to stop-bar det \n")
     
-# =============================================================================
-# match events: advance det to stop-bar det
-# =============================================================================
+    thru_match_initial = [] # initial candidate match pairs of adv to left-turn
 
-thru_match_initial = []
-
-for i in sorted(set_id_adv):
-    if i not in seen_adv_id: # consider only thru moving vehicles
+    for i in set_id_adv_look:
         adv_time = adf[adf.ID == i].TimeStamp
         adv_lane = adf[adf.ID == i].Lane.values[0].astype(int)
-        adv_sca = adf[adf.ID == i].SCA.values[0]
-        
+        set_id_stop_look = set(sdf[sdf.Lane == adv_lane].ID)
+    
         thru_candidate = {}
-        for j in sorted(set_id_stop):
+        for j in set_id_stop_look:        
             stop_time = sdf[sdf.ID == j].TimeStamp
             
             if stop_time.values[0] > adv_time.values[0]: # look forward in timestamp
-                stop_lane = sdf[sdf.ID == j].Lane.values[0].astype(int)
                 stop_sca = sdf[sdf.ID == j].SCA.values[0]
-                
                 tt_adv_stop = (Vector(stop_time) - Vector(adv_time)).pop() # travel time from adv to stop-bar
-                num_lane_change = abs(stop_lane - adv_lane)
                 
-                if tt_adv_stop < tt_thru_min or tt_adv_stop > tt_thru_max or num_lane_change > 1:
+                if tt_adv_stop < tt_thru_min or tt_adv_stop > tt_thru_max:
                     pass
                 else:
                     if stop_sca == 'RG': # if vehicle stops at stop bar
@@ -158,58 +258,80 @@ for i in sorted(set_id_adv):
                     if diff_tt_thru == 0: diff_tt_thru = 0.1 # avoid zero division error
                     
                     thru_match_strength = round(1/diff_tt_thru, 2)
-                    thru_candidate[j] = list([i, thru_match_strength, num_lane_change])
+                    thru_candidate[j] = list([i, thru_match_strength])
                     
         print(i, ":", thru_candidate)
             
         if len(thru_candidate) != 0: # consider only non-empty candidate matches
             thru_match_initial.append(thru_candidate)
+                
+    return thru_match_initial
 
-# convert list of nested dictionaries to flat list
-thru_match_flat = []
-for candidate in thru_match_initial:
-    for key, value in candidate.items():
-        thru_match_flat.append({key: value})
-        
-thru_match_final = {}
-seen_stop_id, seen_adv_id = [], []
-seen_match_strength_stop, seen_match_strength_adv = {}, {}
+# =============================================================================
+# analysis of adv-stop match pairs
+# =============================================================================
 
-# function to update match pairs and seen match strength at stop-bar and adv
-def updateMatchingProcess():
-    thru_match_final[current_stop_id] = list([current_adv_id, current_match_strength, current_lane_change])
-    seen_match_strength_stop[current_stop_id] = current_match_strength
-    seen_match_strength_adv[current_adv_id] = current_match_strength
+thru_match_initial = processCandidateMatchesAdvStop()
+thru_match_final = processFinalMatchPairs(thru_match_initial)
 
-for item in thru_match_flat:
-    key = list(item.keys())
-    value = list(item.values())[0]
-    
-    current_stop_id = key[0]
-    current_adv_id = value[0]
-    current_match_strength = value[1]
-    current_lane_change = value[2]
-    
-    if current_stop_id not in seen_stop_id:
-        if current_adv_id not in seen_adv_id:
-            seen_stop_id.append(current_stop_id)
-            seen_adv_id.append(current_adv_id)
-            updateMatchingProcess()
-        else: # current adv id already seen
-            seen_match_strength = seen_match_strength_adv[current_adv_id]
-            if current_match_strength > seen_match_strength:
-                updateMatchingProcess()
-    else: # current stop id already seen
-        if current_adv_id not in seen_adv_id:
-            seen_match_strength = seen_match_strength_stop[current_stop_id]
-            if current_match_strength > seen_match_strength:
-                updateMatchingProcess()
-        else: # current adv id already seen (look for greater matching strength of adv & stop)
-            seen_match_strength = max(seen_match_strength_stop[current_stop_id], seen_match_strength_adv[current_adv_id])
-            if current_match_strength > seen_match_strength:
-                updateMatchingProcess()
-
-# final match of stop-adv id pairs                
-thru_match = {}
+# final match of adv & stop-bar id pairs
+thru_match_pairs = {}
 for key, value in thru_match_final.items():
-    thru_match[key] = value[0]
+    thru_match_pairs[key] = value[0]
+thru_match_pairs = dict(sorted(thru_match_pairs.items()))
+
+thru_result_pairs = thru_result[num+1]
+
+# correct pairs in both match & result
+TP_pairs = dict(set(thru_match_pairs.items()).intersection(thru_result_pairs.items()))
+
+# pairs in match that were not in result
+FP_pairs = dict(sorted(set(thru_match_pairs.items()).difference(thru_result_pairs.items())))
+
+# pairs in result but not found in match
+FN_pairs = dict(sorted(set(thru_result_pairs.items()).difference(thru_match_pairs.items())))
+
+# stop ids not in both result & match
+set_stop_id_x_result = set_id_stop.difference(set(thru_result_pairs.keys()))
+set_stop_id_x_match = set_id_stop.difference(set(thru_match_pairs.keys()))
+TN_pairs_stop = set_stop_id_x_result.intersection(set_stop_id_x_match)
+
+# adv ids not in both result & match
+set_adv_id_x_result = set_id_adv_look.difference(set(thru_result_pairs.values()))
+set_adv_id_x_match = set_id_adv_look.difference(set(thru_match_pairs.values()))
+TN_pairs_adv = set_adv_id_x_result.intersection(set_adv_id_x_match)
+
+# compute precision & recall
+TP = len(TP_pairs)
+FP = len(FP_pairs)
+FN = len(FN_pairs)
+TN = len(TN_pairs_stop) + len(TN_pairs_adv)
+
+print("Precision: ", round(TP / (TP + FP)*100, 1))
+print("Recall: ", round(TP / (TP + FN)*100, 1))
+
+# append accuracy parameters to dictionary
+thru_accuracy['TP'].append(TP)
+thru_accuracy['FP'].append(FP)
+thru_accuracy['FN'].append(FN)
+thru_accuracy['TN'].append(TN)
+
+# =============================================================================
+# analysis of precision/recall based on full data set
+# =============================================================================
+
+def computeAccuracy(x):
+    TP = sum(x['TP'])
+    FP = sum(x['FP'])
+    FN = sum(x['FN'])
+    TN = sum(x['TN'])
+    
+    print("TP, FP, FN, TN: ", TP, FP, FN, TN)
+    
+    precision = round(TP / (TP + FP)*100, 1)
+    recall = round(TP / (TP + FN)*100, 1)
+    
+    return {'precision': precision, 'recall': recall}
+
+print(computeAccuracy(left_accuracy))
+print(computeAccuracy(thru_accuracy))
